d46b5f2d619a7cef4fe335e627facc25748fb759
albenchris <albenchris00@gmail.com>
2021-01-15 14:04:34 -0600

    pseudocoded script.js file

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+
+// add array to store each hour
+
+
+
+// add array to store task reminders from textareas
+
+
+
+// add function(s) to get and display current date
+
+
+
+// add function(s) to dynamically add .past, .present and .future classes to each hour
+
+
+
+// add function(s) to save task reminders entered into textarea into localStorage
+
+
+
+// add function to render task reminders from localStorage
+
+
+
+// event listeners here if needed

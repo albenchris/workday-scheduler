a0b187b1072bdfaa8fe8c8774562387dd4a2791d
albenchris <albenchris00@gmail.com>
2021-01-16 14:42:32 -0600

    made page layout dynamic to save each row easier

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,6 @@
-// add array to store task reminders from textareas
-var todaysSchedule = [
-    {
-        time: "08",
-        hour: $("#hour-8"),
-        tasks: ""
-    },
-    {
-        time: "09",
-        hour: $("#hour-9"),
-        tasks: ""
-    },
-    {
-        time: "10",
-        hour: $("#hour-10"),
-        tasks: ""
-    },
-    {
-        time: "11",
-        hour: $("#hour-11"),
-        tasks: ""
-    },
-    {
-        time: "12",
-        hour: $("#hour-12"),
-        tasks: ""
-    },
-    {
-        time: "13",
-        hour: $("#hour-13"),
-        tasks: ""
-    },
-    {
-        time: "14",
-        hour: $("#hour-14"),
-        tasks: ""
-    },
-    {
-        time: "15",
-        hour: $("#hour-15"),
-        tasks: ""
-    },
-    {
-        time: "16",
-        hour: $("#hour-16"),
-        tasks: ""
-    },
-    {
-        time: "17",
-        hour: $("#hour-17"),
-        tasks: ""
-    }
-];
+var todaysSchedule = JSON.parse(localStorage.getItem("today")) || [];
+var containerEl = $(".container");
+containerEl.empty();
 
 var currentHour = moment().format("HH");
 
@@ -62,33 +12,101 @@ function displayCurrentDate() {
 
 displayCurrentDate();
 
+// renders hours of 9a - 5p on the page
+for (var hour = 9; hour < 17; hour++) {
+    var index = hour - 9;
 
+    var rowEl = $("<form>")
+        .attr("id", index)
+        .addClass("row time-block hour");
+
+    var div2Col = $("<div>")
+        .addClass("col-md-2 p-5");
+    var hourEl = $("<div>")
+        .text(hour);
+    div2Col.append(hourEl);
+    rowEl.append(div2Col);
+    
+    var div9Col = $("<div>")
+        .addClass("col-md-9 mb-0 p-0");
+    var planEl = $("<textarea>")
+        .attr("id", "input-" + index)
+        .addClass("description");
+    div9Col.append(planEl);
+    rowEl.append(div9Col);
+
+    var div1Col = $("<div>")
+        .attr("id", "save-button-" + index)
+        .addClass("col-md-1 btn saveBtn");
+    var saveIcon = $("<i>")
+        .addClass("fas fa-save fa-lg p-5"); // icon from fontawesome.com
+    div1Col.append(saveIcon);
+    rowEl.append(div1Col);
+
+    containerEl.append(rowEl);
+}
 
 // add function(s) to dynamically add .past, .present and .future classes to each hour
-todaysSchedule.forEach(function(thisTime) {
-    if (thisTime.time > currentHour) {
-        $("textarea").addClass("past");
-    } else if (thisTime.time === currentHour) {
-        $("textarea").addClass("present");
-    } else {
-        $("textarea").addClass("future")
-    }
-    console.log(thisTime.time);
-    console.log(currentHour);
-});
+// todaysSchedule.forEach(function(thisTime) {
+//     if (thisTime.time < currentHour) {
+//         $("textarea").addClass("past");
+//     } else if (thisTime.time === currentHour) {
+//         $("textarea").addClass("present");
+//     } else if (thisTime.time > currentHour) {
+//         $("textarea").addClass("future")
+//     }
+//     // console.log(thisTime.time);
+//     // console.log(currentHour);
+// });
+
+
+
+
 
 // add function(s) to save task reminders entered into textarea into localStorage
-$("#save-button").on("click", function(event) {
-    event.preventDefault();
-});
+
+
+
+function saveTodaysSchedule() {
+    localStorage.setItem("todaysSchedule", JSON.stringify(todaysSchedule));
+};
 
 
 // add function to render task reminders from localStorage
 
 
 
-// event listeners here if needed
 
+
+// function renderTodaysSchedule(todaysSchedule) {
+//     $(".container").empty();
+
+//     for (var i=0; i < todaysSchedule.length; i++) {
+//         var eachHour = $("<form>");
+//         eachHour.text(todaysSchedule[index]);
+
+//         $(".container").append(eachHour);
+//     }
+// };
+
+// renderTodaysSchedule();
+
+
+
+
+// event listeners here if needed
+$("#save-button").on("click", function(event) {
+    event.preventDefault();
+
+    var index = $(this).attr("save-id");
+    var textareaId = "textarea-" + index;
+    var value = $(textareaId).val();
+
+    todaysSchedule[index] = value;
+
+    localStorage.setItem("todaysSchedule", JSON.stringify(todaysSchedule));
+    console.log(todaysSchedule);
+});
 
 // CHECKLIST:
 // change color of hour blocks based on time of day
